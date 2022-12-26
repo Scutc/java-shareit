5eca678528e53114dee145fc54d24b8575b674f4
dmitrykryuchkov <dmitry.kryu4kov@gmail.com>
2022-12-26 16:23:46 +0200

    code style

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -18,7 +18,6 @@ import ru.practicum.shareit.user.service.IUserService;
 import ru.practicum.shareit.user.service.UserMapper;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
